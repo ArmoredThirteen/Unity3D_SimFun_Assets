c88a50a04656760d87dbaf1a1c97e6a71a7a1e78
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2021-05-08 10:20:59 -0700

    Added default liquid settings

--- Sims/TerrainMap/Scripts/DropErosion/LiquidSettings.cs
+++ Sims/TerrainMap/Scripts/DropErosion/LiquidSettings.cs
@@ -8,16 +8,16 @@ namespace ATE.TerrainGen
     [CreateAssetMenu(fileName = "Liquid_", menuName = "ScriptableObjects/SpawnLiquidSettings", order = 1)]
     public class LiquidSettings : ScriptableObject
 	{
-        public int maxLifetime = 10;
-        public float dirtCapacity = 1;
-        public float minDirt = 0.1f;
+        public int maxLifetime = 25;
+        public float dirtCapacity = 4;
+        public float minDirt = 0.01f;
 
         [Range(0,1)]
-        public float grabSpeed;
+        public float grabSpeed = 0.25f;
         [Range(0, 1)]
-        public float dropSpeed;
+        public float dropSpeed = 0.25f;
         [Range(0, 1)]
-        public float evapSpeed;
+        public float evapSpeed = 0.01f;
 		
 	}
 }
