421bde1222a0969e73ac9e9b491ec81ecb40b145
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2021-10-11 17:54:05 -0700

    Added basic inspector buttons for generating full and texture only

---
+++ Sims/TerrainMap/Scripts/TerrainMap/Editor/TerrainMapEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+namespace ATE.TerrainGen
+{
+    [CustomEditor(typeof(TerrainMap))]
+    public class TerrainMapEditor : Editor
+	{
+        private TerrainMap targ;
+
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            targ = (TerrainMap)target;
+
+            EditorGUILayout.Space(20);
+
+
+            EditorGUILayout.LabelField("Generate:");
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Full"))
+                targ.GenerateFull();
+
+            if (GUILayout.Button("Texture"))
+                targ.GenTexture();
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+    }
+}
